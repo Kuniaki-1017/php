1650b9188ba2cbffcbc1fbc8284b1b9dc45b0933
kuniaki <web.t.c.o.k@gmail.com>
2024-02-24 02:22:34 +0900

    db接続

--- dbc.php
+++ dbc.php
@@ -1,1 +1,9 @@
 <?php
+
+$dsn = 'mysql:host=localhost;dbname=blog_app;charset=utf8';
+$user = 'onuma';
+$pass = 'P19If40TddW7V*Vb';
+
+//DB接続用のクラスをインスタンス化：引数にDB情報、user名,パスワードが必要。第四引数でオプションも設定可能
+//mysqlにてユーザー登録を行う際、使用するDBのアクセス権限を付与しておかないと接続できない。※mysqlの権限→データベース→該当のDBをプルダウンで選択→データベースに権限を追加
+$dsh = new PDO($dsn, $user, $pass);
